fcba2a6943fdee14678aed92d387252fdfa28348
ngocphu <ngocphupham682001@gmail.com>
2021-12-23 09:51:09 +0700

    ngocphu

--- src/app/models/Customer.js
+++ src/app/models/Customer.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+// cài đặt mongoose slug 
+const slug = require('mongoose-slug-generator');
+// cài đặt mongoose-delete để softdelete
+const mongooseDelete = require('mongoose-delete')
 const Schema = mongoose.Schema;
 
 const customerSchema = new Schema(
@@ -8,23 +12,23 @@ const customerSchema = new Schema(
         cmnd : { type : Number , required : true }, 
         phone : { type : Number , required : true}, 
         address : { type :  String , required : true},
-  
+        slug: { type: String, slug: "name", unique: true },
     },
     {
         collection : "customer"
     }
 );
-// // region add plugin
-// // cài đặt  mongoose slug
-// mongoose.plugin(slug);
-// // cài đặt mongooseDelete SOFTDELETE
-// // thực hiện override method delete 
-// // để nó mất đi trên trang 
-// // deleteAt để hiển thị thời gian deleted
-// Course.plugin(mongooseDelete, {
-//   overrideMethods: true,
-//   deletedAt: true
-// });
+// region add plugin
+// cài đặt  mongoose slug
+mongoose.plugin(slug);
+// cài đặt mongooseDelete SOFTDELETE
+// thực hiện override method delete 
+// để nó mất đi trên trang 
+// deleteAt để hiển thị thời gian deleted
+customerSchema.plugin(mongooseDelete, {
+  overrideMethods: true,
+  deletedAt: true
+});
 
 
 const customerModel = mongoose.model("customer",customerSchema);
